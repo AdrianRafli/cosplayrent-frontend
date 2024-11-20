0d4e335f01f86cf318f46d5a1ef175f43d30bd57
adrianrafli <adrianrafly20@gmail.com>
2024-11-20 14:43:22 +0700

    add order and review page

---
+++ src/app/order/order.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.page.html',
+  styleUrls: ['./order.page.scss'],
+})
+export class OrderPage implements OnInit {
+  Token:any
+
+  query: string = ''
+  resp:any;
+
+  constructor(private router: Router, public api: ApiService) { }
+
+  ngOnInit() {
+  }
+
+  goToLoginOrProfile(){
+    this.Token = localStorage.getItem('userToken')
+    if (this.Token !== null && this.Token.trim() !== '') {
+      console.log('Token exists');
+      this.router.navigate(['profile'])
+    } else {
+      console.log('Token is empty or does not exist');
+      this.router.navigate(['/login'])
+    }
+  }
+
+  // navigasi
+  goToHome(){
+    this.router.navigate(['/home']);
+  }
+  goToReview(){
+    this.router.navigate(['/review']);
+  }
+}
---
+++ src/app/review/review.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-review',
+  templateUrl: './review.page.html',
+  styleUrls: ['./review.page.scss'],
+})
+export class ReviewPage implements OnInit {
+  // sesuain sama database
+  data: any = { id: '',  review_rating: '', review_picture: '', review_comment: ''};
+  // Token:any
+
+  // resp: any;
+
+
+  rating = 0;
+
+  constructor(public api: ApiService, public router: Router) { }
+
+  ngOnInit() {
+    // this.Token = localStorage.getItem('userToken')
+    // if (this.Token !== null && this.Token.trim() !== '') {
+    //   this.api.verifyAndRetrieve('verifytoken').subscribe((resp) => {
+    //     this.resp = resp;
+    //     if (this.resp.code == "200") {
+    //       this.data = this.resp.data;
+    //       console.log(this.data);
+    //     }
+    //   })
+    // } else {
+    //   console.log('Token is empty or does not exist');
+    //   this.router.navigate(['/login'])
+    // }
+  }
+
+  // nilai rating disimpan di variable rating (number 1-5)
+  setRating(value: number) {
+    this.rating = value;
+    console.log(this.rating);
+  }
+
+  goToOrder(){
+    this.router.navigate(['/order']);
+  }
+
+  
+
+}
