2aff32b42a8db16517285b309a27621099f7b75c
ferdian3456 <mulyadipermadi21@gmail.com>
2025-01-24 22:45:57 +0700

    Fix ionic capacitor setting to build the app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,46 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'cosplayrent',
-  webDir: 'www'
+  appId: 'com.aster.cosplayrent',
+  appName: 'CosplayRent',
+  webDir: 'www',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  }
 };
 
 export default config;
+
+// import { CapacitorConfig } from '@capacitor/cli';
+
+// const config: CapacitorConfig = {
+//   plugins: {
+//     SplashScreen: {
+//       launchShowDuration: 3000,
+//       launchAutoHide: true,
+//       launchFadeOutDuration: 3000,
+//       backgroundColor: "#ffffffff",
+//       androidSplashResourceName: "splash",
+//       androidScaleType: "CENTER_CROP",
+//       showSpinner: true,
+//       androidSpinnerStyle: "large",
+//       iosSpinnerStyle: "small",
+//       spinnerColor: "#999999",
+//       splashFullScreen: true,
+//       splashImmersive: true,
+//       layoutName: "launch_screen",
+//       useDialog: true,
+//     },
+//   },
+// };
+
+// export default config;
